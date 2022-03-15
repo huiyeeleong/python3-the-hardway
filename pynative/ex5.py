f2fd21bea47aa56d87ca1f2330f1c9a02e8c9b38
Hui Yee Leong <66815986+huiyeeleong@users.noreply.github.com>
2022-03-15 22:37:00 +1100

    Updated Stack

--- pynative/ex5.py
+++ pynative/ex5.py
@@ -1,8 +1,5 @@
 # Write a function to return True if the first and last number of a given list is same. If numbers are different then return False.
 
-from distutils.command.build_scripts import first_line_re
-
-
 def num (numberlist):
     numberx = numberlist[0]
     numbery = numberlist[-1]
