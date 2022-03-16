07d140525fd4ef4e8968d5bd37e220405ed4495c
Hui Yee Leong <66815986+huiyeeleong@users.noreply.github.com>
2022-03-16 11:38:06 +1100

    Updated Stack

---
+++ pynative/python-basics/ex7.py
@@ -0,0 +1,7 @@
+# Write a program to find how many times substring “Emma” appears in the given string.
+# Use string method count().
+
+str_x = "Emma is good developer. Emma is a writer"
+
+x = str_x.count("Emma")
+print(x)
